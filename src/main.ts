3c7f15827ab84c8716eec2e6acb5e5eebb4d9f99
Ignose <ihpares@gmail.com>
2025-06-04 14:28:30 -0400

    BuskerSolver

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
-import { myMp, print } from "kolmafia";
+import { Args } from "grimoire-kolmafia";
+import { findTopBusksFast, generateOne, printBuskResult } from "./buskingtest2";
+import { NumericModifier, numericModifiers } from "libram";
 
-export function checkMP(): string {
-  if (myMp() < 200) {
-    return "Your MP is less than 200.";
-  } else {
-    return "Your MP is greater than or equal to 200.";
+const args = Args.create(
+  "Beret Busk Tester",
+  'Be good, be kind',
+  {
+    modifiers: Args.string({
+      help: "Numeric Modifier to check",
+      default: "Meat Drop"
+    })
   }
+)
+
+function toModifierArray(input: string): NumericModifier[] {
+  return input
+    .split(",")
+    .map((s) => s.trim())
+    .filter((s): s is NumericModifier => s in numericModifiers);
 }
 
+const modifier = toModifierArray(args.modifiers);
+
+const result = findTopBusksFast(generateOne, modifier);
+
 export function main(): void {
-  print(checkMP());
+  printBuskResult(result);
 }
